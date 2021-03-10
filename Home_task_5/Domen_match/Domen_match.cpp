d05802eb25ce52fcd95d3d3bc2dd2b05911ee0fd
KlopovAleksey <55539502+KlopovAleksey@users.noreply.github.com>
2021-03-10 21:55:30 +0300

    Update Domen_match.cpp
    new regex

--- Home_task_5/Domen_match/Domen_match.cpp
+++ Home_task_5/Domen_match/Domen_match.cpp
@@ -17,8 +17,8 @@ bool is_valid(std::string & str) {
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    const std::regex regular(R"(\w+\.?\w*(@\w+\.[[:alpha:]]+))");
-
+    //const std::regex regular(R"(\w+\.?\w*(@\w+\.[[:alpha:]]+))");
+    const std::regex regular(R"([ [:alnum:]]\w+\.?\w*(@\w+\.[[:alpha:]]+))");
     std::ofstream out("D:\\Alex\\FOPF\\Прога\\C++\\Семинар5\\output.txt");
     std::ifstream in("D:\\Alex\\FOPF\\Прога\\C++\\Семинар5\\input.txt");
 
@@ -29,14 +29,14 @@ int main()
         std::cout << "строка: \t" << tmp << std::endl;
         if (std::regex_search(tmp, regular)) {
             const std::vector<std::smatch> full_match{
-            std::sregex_iterator{tmp.begin(), tmp.end(), regular},
+            std::sregex_iterator{tmp.begin(), tmp.end(), regular}, // Mark `0` here i.e. whole regex match
             std::sregex_iterator{}
             };
             for (auto i : full_match){
                 std::cout << i.str(0) << "\t" << i.str(1) << '\n';
             }
         }
-        else { std::cout << "\n не нашёл \n "; }
+        else { std::cout << "не нашёл \n "; }
 
 
     }
