751ea2e73226edfb46adb44987e9a0090cfdefb6
KlopovAleksey <55539502+KlopovAleksey@users.noreply.github.com>
2021-03-10 21:39:19 +0300

    Create Domen_match.cpp

---
+++ Home_task_5/Domen_match/Domen_match.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <regex>
+#include <fstream>
+#include <cstring>
+#include <cassert>
+#include <string_view>
+
+bool is_valid(std::string & str) {
+
+    return true;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    const std::regex regular(R"(\w+\.?\w*(@\w+\.[[:alpha:]]+))");
+
+    std::ofstream out("D:\\Alex\\FOPF\\Прога\\C++\\Семинар5\\output.txt");
+    std::ifstream in("D:\\Alex\\FOPF\\Прога\\C++\\Семинар5\\input.txt");
+
+
+    while (in) {
+        std::string tmp;
+        std::getline(in, tmp);
+        std::cout << "строка: \t" << tmp << std::endl;
+        if (std::regex_search(tmp, regular)) {
+            const std::vector<std::smatch> full_match{
+            std::sregex_iterator{tmp.begin(), tmp.end(), regular},
+            std::sregex_iterator{}
+            };
+            for (auto i : full_match){
+                std::cout << i.str(0) << "\t" << i.str(1) << '\n';
+            }
+        }
+        else { std::cout << "\n не нашёл \n "; }
+
+
+    }
+    return 0;
+}
