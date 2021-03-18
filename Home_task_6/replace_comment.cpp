cc21b490bf65c42116944dd55cf8515011e079a5
KlopovAleksey <55539502+KlopovAleksey@users.noreply.github.com>
2021-03-18 10:40:22 +0300

    Update replace_comment.cpp

--- Home_task_6/replace_comment.cpp
+++ Home_task_6/replace_comment.cpp
@@ -1,23 +1,52 @@
-
+#include <iostream>
 #include <string>
-#include <regex>
 #include <fstream>
 #include <filesystem>
 
 int main()
 {
+    auto path1 = std::filesystem::path("D:\\Alex\\FOPF\\Прога\\C++\\Семинар6\\test.txt"); 
+    std::ofstream fout("D:\\Alex\\FOPF\\Прога\\C++\\Семинар6\\output.txt");
+    std::ifstream fin(path1.string(), std::ios::in);
 
-    auto path1 = std::filesystem::path("D:/Alex/FOPF/Прога/C++/Семинар6/test.txt"); 
-    std::ofstream out("D:\\Alex\\FOPF\\Прога\\C++\\Семинар6\\output.txt");
-    const std::regex regular(R"(/*.**/)");    
-    std::fstream fin(path1.string(), std::ios::in);
+    bool in_string = false; // внутри открытой строки
+    bool multiline_comment = false; // внутри многострочного(опционально) комментария
+
 
     while (fin) {
-        std::string tmp;
-        std::string tmp1;
-        std::getline(fin, tmp);
-        std::regex_replace(tmp1, tmp.begin(), tmp.end(), regular, "");
-        out << tmp1 << "\n";
+        std::string tmp_in;
+        std::string tmp_out;
+        std::getline(fin, tmp_in);
+        for (size_t i = 0; i < tmp_in.length(); i++) {
+            if (tmp_in[i] == '/' && tmp_in[i + 1] == '*') { // многострочный(опционально) коммент начался
+                multiline_comment = true;
+                i++;
+                continue;
+            }
+
+            if ( tmp_in[i + 1] == '/' && tmp_in[i] == '*') { // многострочный(опционально) коммент закончился
+                if (multiline_comment = true) { // если поставить это условие третьим в предыдущий if, то будет ошибка по непонятной причине
+                    multiline_comment = false;
+                    i++;
+                    continue;
+                }
+            }
+
+            if (tmp_in[i] == '"') { // состояние открытой/закрытой строки
+                in_string = !in_string;
+            }
+
+            if (tmp_in[i] == '/' && tmp_in[i + 1] == '/' && !in_string) {   // внтури открытой строки за коммент не считаем
+                break;
+            }
+
+            if (!multiline_comment) // запись в строку вывода поэлементно
+                tmp_out += tmp_in[i];
+        }
+        
+        if (tmp_out != "\n") // запись строки без комментов в файл
+                fout << tmp_out << std::endl;
+        
     }
     
     return 0;
